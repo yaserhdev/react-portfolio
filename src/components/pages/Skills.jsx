1550a93c9f030465acedf7ef2346c9ef85f01d24
Raja Yaser Hussain <yaser@Yasers-MacBook-Pro.local>
2024-05-12 22:52:32 -0400

    Added skills and experience section

--- src/components/pages/Skills.jsx
+++ src/components/pages/Skills.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
 import '../../styles/Skills.css';
 import { getImageUrl } from '../../utils/helpers';
+import skills from "../data/skills.json";
+import experience from "../data/experience.json";
 
 export default function Skills() {
     return (
-        <section className='skills-container'>
-            <h1 className='skills-title'>Skills</h1>
-            <div className='skills-content'>
-                    <p>Skills go here...</p>
+        <section className='skills-experience-container'>
+            <h1 className='skills-experience-title'>Skills and Experience</h1>
+            <div className='skills-experience-content'>
+                <div className="skills">
+                    {skills.map((skill, id) => {
+                        return (
+                            <div key={id} className="skill">
+                                <div className="skill-image">
+                                    <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                                </div>
+                                <p className="skill-title">{skill.title}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+                <ul className="experience">
+                    {experience.map((experienceItem, id) => {
+                        return <li key={id} className='experienceItem'>
+                            <img src={getImageUrl(experienceItem.imageSrc)} alt={`${experienceItem.organization} logo`} className="experience-image" />
+                            <div className="experience-details">
+                                <h3>{`${experienceItem.role}, ${experienceItem.organization}`}</h3>
+                                <p>{`${experienceItem.start} - ${experienceItem.end}`}</p>
+                                <ul>{experienceItem.experiences.map((experience, id) => {
+                                    return <li key={id}>{experience}</li>
+                                })}
+                                </ul>
+                            </div>
+                        </li>
+                    })}
+                </ul>
             </div>
         </section>
     );
